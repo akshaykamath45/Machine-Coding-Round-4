da0d4f0ba4339f02647dc42e5a7de594a4652780
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-08-26 04:06:00 +0530

    feat : loading data on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,39 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { DataContext } from "../contexts/DataContext";
-
-export const Home=()=>{
-const {data}=useContext(DataContext)
-console.log(data);
-    return (
-        <div>
-            <h4>Home Page</h4>
-        </div>
-    )
-}
+import "./Home.css";
+export const Home = () => {
+  const { data } = useContext(DataContext);
+  console.log(data);
+  return (
+    <div>
+      <h4>Latest Posts</h4>
+      {data.posts.map((post) => {
+        return (
+          <div className="landing-card">
+            <div className="counter">
+              <button>+</button>
+              <h2>{post.upvotes - post.downvotes}</h2>
+              <button>-</button>
+            </div>
+            <div className="card-content">
+              <p>Posted by {post.username}</p>
+              <h2>{post.post}</h2>
+              <div className="post-tags">
+                {post.tags.map((tag) => {
+                  return <p>{tag}</p>;
+                })}
+              </div>
+              <p>{post.postDescription}</p>
+              <hr />
+              <div className="card-footer">
+                <div>Comment</div>
+                <div>Share</div>
+                <div>Footer</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
