e413ea0efe75b00bc8a38f1920f12150d54a03b7
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-08-26 03:23:46 +0530

    style : designing grid layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
-
+import { Sidebar } from "./components/Sidebar";
 function App() {
   return (
     <div className="App">
-      <h1>Forum App</h1>
+      <div className='layout'>
+        <div className='header'>
+          My Forum
+        </div>
+        <div className='sidebar'>
+           <div>Home</div>
+           <div>Explore</div>
+           <div>Bookmark</div>
+           <div>Profile</div>
+        </div>
+        <div className='main'>
+            <div className='card'>card1</div>
+            <div className='card'>card2</div>
+        </div>
+      <div className='sort-by'>
+        <div className='sort-card'>Sort By</div>
+      </div>
+      <div className='profile'>
+        <div className='profile-card'>Profile</div>
+      </div>
+      </div>
+    
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+
+export const Sidebar=()=>{
+    return(
+        <div className='sidebar'>
+           <div>Home</div>
+           <div>Explore</div>
+           <div>Bookmark</div>
+           <div>Profile</div>
+        </div>
+    )
+}
