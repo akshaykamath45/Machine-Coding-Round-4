b6b36514dffa34d455fc0cc5547aaa631fdcbd8f
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-08-26 05:49:39 +0530

    feat : implementing comment section

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,21 +1,19 @@
-import { createContext,useState,useEffect} from "react";
-import {forumData} from '../data/Data'
-export const DataContext=createContext();
+import { createContext, useState, useEffect } from "react";
+import { forumData } from "../data/Data";
+export const DataContext = createContext();
 
-export const DataProvider=({children})=>{
-    // const getDataFromLocalStorage=localStorage.getItem('forumData') ? JSON.parse(localStorage.getItem('forumData')) : forumData;
+export const DataProvider = ({ children }) => {
+  // const getDataFromLocalStorage=localStorage.getItem('forumData') ? JSON.parse(localStorage.getItem('forumData')) : forumData;
 
-    const [data,setData]=useState(forumData)
+  const [data, setData] = useState(forumData);
 
-    // useEffect(()=>{
-    //     localStorage.setItem('forumData',JSON.stringify(data));
-    // },[])
-    const value={
-        data,setData
-    }
-    return(
-        <DataContext.Provider value={value} >
-            {children}
-        </DataContext.Provider>
-    )
-}
+  // useEffect(()=>{
+  //     localStorage.setItem('forumData',JSON.stringify(data));
+  // },[])
+  const value = {
+    data,
+    setData,
+  };
+
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
